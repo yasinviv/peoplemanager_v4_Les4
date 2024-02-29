e755e71c3100d862a2fbfe5b8fd045e152b27ef1
yasinviv <159425350+yasinviv@users.noreply.github.com>
2024-02-29 11:29:26 +0100

    Initial commit

---
+++ PeopleManager.Ui.Mvc/Controllers/PeopleController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeopleManager.Ui.Mvc.Core;
+
+namespace PeopleManager.Ui.Mvc.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private readonly PeopleManagerDbContext _peopleManagerDbContext;
+
+        public PeopleController(PeopleManagerDbContext peopleManagerDbContext)
+        {
+            _peopleManagerDbContext = peopleManagerDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var people = _peopleManagerDbContext.People.ToList();
+
+            return View(people);
+        }
+    }
+}
---
+++ PeopleManager.Ui.Mvc/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PeopleManager.Ui.Mvc.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public string? Email { get; set; }
+    }
+}
